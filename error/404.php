cfffe6cbb59698b6e1552e7c5ecfbe7ce3db5871
irfan-deva <irfandeva@gmail.com>
2023-12-30 22:59:56 +0530

    added content

--- error/404.php
+++ error/404.php
@@ -1,2 +1,8 @@
 <?php
-echo "<h1>404 , not found</h1>";
+loadPartial('header'); ?>
+
+<div class="content">
+  <?php echo "<p class='err'>404 , not found</p>"; ?>
+</div>
+<?php
+loadPartial('footer');
